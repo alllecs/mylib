829832330e39ec7dcfb512c3b2404f78089a579d
Smirnov Alexander <alllecs@yandex.ru>
2015-01-25 18:10:46 +0300

    Рабочая версия программы

--- kor.c
+++ kor.c
@@ -6,22 +6,44 @@
 int main(void)
 {
 	int ret;
-	int a;
-	double b;
-
-	printf("Введите число\n");
-	ret = scanf("%d", &a);
+	double a, b, c, D;
+	double x1, x2;
+	printf("Уравнение a*x^2 + b*x + c = 0\n");
+	printf("Введите коэффициент 'a'\n");
+	ret = scanf("%lf", &a);
 
 	if (ret != 1)
 		return 2;
 
-	if (a < 0) {
-		printf("Указано отрицательное число\n");
-		return 1;
+	printf("Введите коэффициент 'b'\n");
+	ret = scanf("%lf", &b);
+
+	if (ret != 1)
+		return 2;
+
+	printf("Введите коэффициент 'c'\n");
+	ret = scanf("%lf", &c);
+
+	if (ret != 1)
+		return 2;
+
+	D = pow(b,2) - (4 * a * c);
+	if (D < 0) {
+		printf("Нет действительных корней уравнения\n");
+		return 3;
 	}
-
-	b = sqrt(a);
-	printf("%.2f\n", b);
-
+	if (D == 0) {
+		x1 = ((- b) + sqrt(D)) / (2 * a);
+		printf("Один корень уравнения %.2f\n", x1);
+		return 4;
+	}
+	if (D > 0) {
+		x1 = ((- b) + sqrt(D)) / (2 * a);
+		x2 = ((- b) - sqrt(D)) / (2 * a);
+		printf("Первый корень уравнения %.2f,"
+			" второй корень уравнения %.2f\n",
+			x1, x2);
+		return 5;
+	}
 	return 0;
 }
