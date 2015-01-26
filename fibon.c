987049624a2c177f0714ca5d4d19c8f21e75b449
Smirnov Alexander <alllecs@yandex.ru>
2015-01-26 15:06:19 +0300

    Исправленная версия программы

--- fibon.c
+++ fibon.c
@@ -2,44 +2,29 @@
 
 int main(void)
 {
-	double i, j, k;
-	double f, p;
+	int n, m;
 	int ret;
+	double i, j, k, f;
 
-
-	ret = scanf("Введите погрешность в формате 0.x %lf", &p);
+	printf("Введите номер члена ряда Фибоначчи\n");
+	ret = scanf("%d", &n);
 
 	if (ret != 1)
 		return 2;
 
 	j = 1;
 	i = 1;
-	printf("%.2f\n", i);
-	printf("%.2f\n", j);
-	while (j < 100) {
+	m = 1;
+	while (m < n) {
+		f = j / i;
 		k = i + j;
 		i = j;
 		j = k;
-
-		if (j > 100)
-			break;
-
-		printf("%.2f\n", j);
-
-		f = i / j;
-
-		switch (p) {
-			case 0.01:
-				printf("Коэффициент 'золотого сечения' %.3f\n", f);
-				break;
-			case 0.001:
-				printf("Коэффициент 'золотого сечения' %.4f\n", f);
-				break;
-			case 0.0001:
-				printf("Коэффициент 'золотого сечения' %.5f\n", f);
-
-		}
+		m += 1;
 	}
-
+	printf("%.0f\n", i);
+	k = j / i;
+	printf("%.3f\n", k);
+	printf("%.6f\n", (k - f));
 	return 0;
 }
