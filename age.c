49e199d1b608cde923f2b12bc4ea062de3108cb0
Smirnov Alexander <alllecs@yandex.ru>
2015-02-03 13:16:26 +0300

    Исправлено форматирование

--- age.c
+++ age.c
@@ -1,52 +1,55 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 char *age(int i)
 {
 	switch (i % 10) {
 		case 1:
-			return"год";
+			return "год";
 		case 2:
 		case 3:
 		case 4:
-			return"года";
+			return "года";
 	}
-	return"лет";
+	return "лет";
 }
 
 int main(void)
 {
 	int i;
 	int x, y;
+	const int sov = 18;
+	const int pen = 60;
 
 	printf("Введите возраст\n");
 	scanf("%d", &i);
 
-	if(i < 1 || i > 100) {
+	if (i < 1 || i > 100) {
 		printf("Неправильно указан возраст\n");
 		return 3;
 	}
 
 	printf("Ваш возраст %d %s\n", i, age(i));
 
-	x = fabs(i - 18);
-	if (i < 18) {
+	x = abs(i - sov);
+	if (i < sov) {
 		printf("Вам до совершеннолетия %d %s\n", x, age(x));
 	}
-	if (i > 18) {
+	if (i > sov) {
 		printf("Вы совершеннолетний %d %s\n", x, age(x));
 	}
-	if (i == 18)
+	if (i == sov)
 		printf("Вы совершеннолетний\n");
 
-	y = fabs(i - 60);
-	if (i < 60) {
+	y = abs(i - pen);
+	if (i < pen) {
 		printf("Вам до пенсии %d %s\n", y, age(y));
 	}
-	if (i > 60) {
+	if (i > pen) {
 		printf("Вы на пенсии %d %s\n", y, age(y));
 	}
-	if (i == 60)
+	if (i == pen)
 		printf("Вы пенсионер\n");
 
 	return 0;
