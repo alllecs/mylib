1a02b87e810fd90ffef789009a434a31c8975bb2
Smirnov Alexander <alllecs@yandex.ru>
2015-02-03 17:24:42 +0300

    Добавлен ввод с коммандной строки

--- age.c
+++ age.c
@@ -15,15 +15,22 @@ char *age(int i)
 	return "лет";
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int i;
 	int x, y;
 	const int sov = 18;
 	const int pen = 60;
 
-	printf("Введите возраст\n");
-	scanf("%d", &i);
+	if (argc == 2)
+		sscanf(argv[1], "%d", &i);
+
+	if (argc != 2) {
+		printf("Введите возраст\n");
+		scanf("%d", &i);
+	}
+
+
 
 	if (i < 1 || i > 100) {
 		printf("Неправильно указан возраст\n");
@@ -33,24 +40,30 @@ int main(void)
 	printf("Ваш возраст %d %s\n", i, age(i));
 
 	x = abs(i - sov);
-	if (i < sov) {
-		printf("Вам до совершеннолетия %d %s\n", x, age(x));
-	}
-	if (i > sov) {
-		printf("Вы совершеннолетний %d %s\n", x, age(x));
-	}
+
 	if (i == sov)
 		printf("Вы совершеннолетний\n");
+	else {
+		if (i < sov) {
+			printf("Вам до совершеннолетия ");
+		}
+		if (i > sov) {
+			printf("Вы совершеннолетний ");
+		}
+		printf("%d %s\n", x, age(x));
+	}
 
 	y = abs(i - pen);
-	if (i < pen) {
-		printf("Вам до пенсии %d %s\n", y, age(y));
-	}
-	if (i > pen) {
-		printf("Вы на пенсии %d %s\n", y, age(y));
-	}
 	if (i == pen)
 		printf("Вы пенсионер\n");
-
+	else {
+		if (i < pen) {
+			printf("Вам до пенсии ");
+		}
+		if (i > pen) {
+			printf("Вы на пенсии ");
+		}
+			printf("%d %s\n", y, age(y));
+	}
 	return 0;
 }
