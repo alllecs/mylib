503ecad1ebbce933e68b3d0e5687825109f1acc0
Smirnov Alexander <alllecs@yandex.ru>
2015-02-03 17:25:33 +0300

    Добавлен ввод с командной строки. Исправлен цикл.

--- tranfer.c
+++ tranfer.c
@@ -1,42 +1,61 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int system, numb, rest;
 	int i = 0;
 	int k;
 	int mass[99];
 	double binnumb, bin;
+	double x;
 
-	printf("Введите десятичное число\n");
-	scanf("%lf", &binnumb);
+	if (argc == 2)
+		sscanf(argv[1], "%lf", &binnumb);
 
-	printf("Введите систему счисления\n");
-	scanf("%d", &system);
+	if (argc != 2) {
+		printf("Введите десятичное число\n");
+		scanf("%lf", &binnumb);
 
-	if (system < 2 || system > 16) {
-		printf("Неправильно введена система счисления\n");
-		return 3;
+		printf("Введите систему счисления\n");
+		scanf("%d", &system);
+
+		if (system < 2 || system > 16) {
+			printf("Неправильно введена система счисления\n");
+			return 3;
+		}
 	}
 
 	numb = (int)binnumb;
 	bin = binnumb - (int)binnumb;
 
-	if (numb >=1)
+	if (numb >= 1) {
 		while (numb > 0) {
 			rest = numb % system;
 			numb = numb / system;
 			mass[i++] = rest;
+		}
 	}
-
 	for (k = i - 1; k > - 1; k--) {
 		printf("%X", mass[k]);
 	}
 	printf(".");
+
 	i = 0;
+
 	if (bin < 1 && bin > 0) {
 		rest = 0;
+		x = 1;
+		while (x > 0.0001) {
+			x = x / 2;
+			if (bin >= x) {
+				bin = bin - x;
+				rest = 1;
+			} else
+				rest = 0;
+			mass[i++] = rest;
+		}
+#if 0
 		while (bin > 0.0001) {
 			if (bin >= 0.5) {
 				bin = bin - 0.5;
@@ -60,7 +79,7 @@ int main(void)
 			mass[i++] = rest;
 			break;
 		}
-
+#endif
 		for (i = 0; i < 3; i++) {
 			printf("%X", mass[i]);
 		}
