a53b88a98ad06c16467a2ca5b420ded79d890c24
Smirnov Alexander <alllecs@yandex.ru>
2015-02-03 14:15:48 +0300

    Исправлен вывод дробного числа

--- tranfer.c
+++ tranfer.c
@@ -4,58 +4,57 @@
 int main(void)
 {
 	int system, numb, rest;
-	int i;
+	int i = 0;
 	int k;
 	int mass[99];
-	int ret;
 	double binnumb, bin;
 
 	printf("Введите десятичное число\n");
-	ret = scanf("%lf", &binnumb);
+	scanf("%lf", &binnumb);
 
 	printf("Введите систему счисления\n");
-	ret = scanf("%d", &system);
+	scanf("%d", &system);
 
 	if (system < 2 || system > 16) {
 		printf("Неправильно введена система счисления\n");
 		return 3;
 	}
 
-	if (ret != 1)
-		return 2;
-
 	numb = (int)binnumb;
 	bin = binnumb - (int)binnumb;
 
-	while (numb > 0) {
-		if (bin < 1) {
-			mass[i++] = 0;
-			if (bin > 0.5) {
+	if (bin < 1 && bin > 0) {
+		rest = 0;
+		while (bin > 0.0001) {
+			if (bin >= 0.5) {
 				bin = bin - 0.5;
 				rest = 1;
 			} else
 				rest = 0;
 			mass[i++] = rest;
 
-			if (bin > 0.25) {
+			if (bin >= 0.25) {
 				bin = bin - 0.25;
 				rest = 1;
 			} else
 				rest = 0;
 			mass[i++] = rest;
 
-			if (bin > 0.125) {
+			if (bin >= 0.125) {
 				bin = bin - 0.125;
 				rest = 1;
 			} else
 				rest = 0;
 			mass[i++] = rest;
+			break;
 		}
-		if (numb > 1) {
+	}
+
+	if (numb >=1)
+		while (numb > 0) {
 			rest = numb % system;
 			numb = numb / system;
 			mass[i++] = rest;
-		}
 	}
 
 	for (k = i - 1; k > - 1; k--) {
