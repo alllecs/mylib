586a224edb8d62e1c33c14ba7963ec66a2df139f
Smirnov Alexander <alllecs@yandex.ru>
2015-02-04 14:44:19 +0300

    Добавлен вывод количества символов, строк, слов.

--- get.c
+++ get.c
@@ -4,23 +4,31 @@
 int main(void)
 {
 	char c;
-	int n = 0;
 	int i = 0;
-	int k = 0;
+	int symbol = 0;
+	int word = 0;
+	int line = 0;
 	char buf[99];
 
 	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '	' || c == '\n') {
+			word++;
+			if (c == '\n')
+				line++;
+			c = '\n';
+		}
 		buf[i++] = c;
-//		putchar(c);
-		n++;
-		if (c == '\n')
-			k++;
+
+		symbol++;
 	}
 
 
-	for (i = 0;i < n; i++)
+	for (i = 0;i < symbol; i++)
 		putchar(buf[i]);
 
-	printf("%d\n", n);
-	printf("%d\n", k);
+	printf("\nNumber of symbols = %d\n", symbol);
+	printf("Number of words = %d\n", word);
+	printf("Number of lines = %d\n", line);
+
+	return 0;
 }
