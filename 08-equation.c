1dfdc5af0d9adcfe53271f4e6a6605b52e57f701
Smirnov Alexander <alllecs@yandex.ru>
2015-03-03 12:08:40 +0300

    Рабочая версия программы

--- 08-equation.c
+++ 08-equation.c
@@ -6,28 +6,38 @@ double f(double x)
 	return (x - exp(-x));
 }
 
-double del(double a, double b, double c)
+double del(double a, double b, double e)
 {
-	double k;
+	double c;
+	if (f(a) > 0 || f(b) < 0) {
+		printf("Значения функции на концах отрезка одинакового знака\n");
+		return 0;
+	}
 
-	while (fabs(b - a) > c) {
-		k = (a + b) / 2;
-		if ((f(a) * f(b)) < 0) {
-			b = k;
+	while (f(fabs((a + b) / 2)) > e) {
+		c = (a + b) / 2;
+		if (f(c) < 0) {
+			a = c;
 		} else {
-			a = k;
+			b = c;
 		}
 	}
 
-	return k;
+	return c;
 }
 
 void main()
 {
-	double x, c;
-	printf("Введите точность = ");
-	scanf("lf", &c);
-	x = del(-3.123, 4.234, c);
+	double x, e;
+/*	int a, b;
+	printf("Введите левую границу отрезка (а) = ");
+	scanf("%d", &a);
+	printf("Введите правую границу отрезка (b) = ");
+	scanf("%d", &b);
+*/	printf("Введите точность = ");
+	scanf("lf", &e);
+
+	x = del(-1, 3, e);
 	printf("x = %lf\n", f(x));
 
 }
