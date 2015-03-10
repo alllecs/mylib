26e06288a9125c0896e130bcbceed804317fdc43
Smirnov Alexander <alllecs@yandex.ru>
2015-03-10 14:00:06 +0300

    Добавлено условие в цикл заполнения таблицы

--- 09-array.c
+++ 09-array.c
@@ -5,9 +5,22 @@
 
 unsigned int array[COMMANDS][COMMANDS];
 
-void rand_my(int a)
+int array_my(unsigned int *array)
 {
-	a = rand();
+	int i, j;
+
+	printf("------\n");
+	for (i = 0; i < COMMANDS; i++) {
+		for (j = 0; j < COMMANDS; j++) {
+			printf("%d\t", array[i * COMMANDS + j]);
+		}
+		printf("\n");
+	}
+}
+
+int rand_my(int a)
+{
+	a = rand() % 3;
 	if(a == 2) {
 		a = 3;
 	}
@@ -18,41 +31,53 @@ void tab(int a)
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++) {
-		for (j = 0; j < 8; j++) {
+	a = 0;
+	for (i = 0; i < COMMANDS; i++) {
+		for (j = 0; j < COMMANDS; j++) {
+
+			if (j < i) {
+
+				a = rand_my(a);
+				if (a == 1) {
+					array[i][j] = a;
+					array[j][i] = a;
+				}
+				if (a == 3) {
+					array[i][j] = a;
+					array[j][i] = 0;
+				}
+				if (a == 0) {
+					array[i][j] = a;
+					array[j][i] = 3;
+				}
+			}
+
 			if (i == j) {
 				array[i][j] = 0;
 			}
-			if (rand_my(a) == 1) {
-				array[i][j] = rand_my(a);
-				array[j][i] = rand_my(a);
-			}
-			if (rand_my(a) == 3) {
-				array[i][j] = rand_my(a);
-				array[j][i] = 0;
-			}
-			if (rand_my(a) == 0) {
-				array[i][j] = rand_my(a);
-				array[j][i] = 3;
-			}
-			printf("%d", array[i][j]);
+
+		}
 	}
 }
 
 
-void main(void)
+int main(void)
 {
 	int i, j;
 	int sum;
 	int a;
+	char max[COMMANDS];
 
-	sum = 0;
+	array_my(&array[0][0]);
+
+	a = 0;
 	tab(a);
-	for (i = 0; i < 8; i++) {
-		for (j = 0; j < 8; j++) {
+	array_my(&array[0][0]);
+	for (i = 0; i < COMMANDS; i++) {
+		sum = 0;
+		for (j = 0; j < COMMANDS; j++) {
 			sum = sum + array[i][j];
 		}
-		printf("Команда %d набрала %d очков\n", i, sum);
+		printf("Команда %d набрала %d очков\n", i + 1, sum);
 	}
-
 }
