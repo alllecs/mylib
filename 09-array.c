a45805241ff2e4636a388ab90911d75b18917629
Smirnov Alexander <alllecs@yandex.ru>
2015-03-11 14:32:07 +0300

    Рабочая версия программы

--- 09-array.c
+++ 09-array.c
@@ -5,17 +5,50 @@
 
 unsigned int array[COMMANDS][COMMANDS];
 
+char *cmd_name[] = {
+	"Spartak",
+	"Dinamo",
+	"Zenit",
+	"Chelsie",
+	"Spartak-Paris",
+	"Dinamo-Berlin",
+	"Zenit-Amsterdam",
+	"Loko",
+};
+
+#if (COMMANDS > 8)
+#error "too many commands"
+#endif
+
+char *point(int i)
+{
+
+	if (i < 10 || i > 20) {
+		switch (i % 10) {
+			case 1:
+				return "очко";
+			case 2:
+			case 3:
+			case 4:
+				return "очка";
+		}
+	}
+	return "очков";
+}
+
+
 int array_my(unsigned int *array)
 {
 	int i, j;
 
-	printf("------\n");
 	for (i = 0; i < COMMANDS; i++) {
+		printf("%-10s\t", cmd_name[i]);
 		for (j = 0; j < COMMANDS; j++) {
-			printf("%d\t", array[i * COMMANDS + j]);
+			printf("%-4d\t", array[i * COMMANDS + j]);
 		}
 		printf("\n");
 	}
+	return 0;
 }
 
 int rand_my(int a)
@@ -27,35 +60,39 @@ int rand_my(int a)
 	return a;
 }
 
-void tab(int a)
+void tab()
 {
-	int i, j;
+	int i, j, a;
 
 	a = 0;
+
 	for (i = 0; i < COMMANDS; i++) {
 		for (j = 0; j < COMMANDS; j++) {
 
-			if (j < i) {
-
-				a = rand_my(a);
-				if (a == 1) {
-					array[i][j] = a;
-					array[j][i] = a;
-				}
-				if (a == 3) {
-					array[i][j] = a;
-					array[j][i] = 0;
-				}
-				if (a == 0) {
-					array[i][j] = a;
-					array[j][i] = 3;
-				}
-			}
-
 			if (i == j) {
 				array[i][j] = 0;
+				break;
 			}
 
+			a = rand_my(a);
+			switch (a) {
+				case 1:
+					array[i][j] = a;
+					array[j][i] = a;
+				break;
+				case 3:
+					array[i][j] = a;
+					array[j][i] = 0;
+				break;
+				case 0:
+					array[i][j] = a;
+					array[j][i] = 3;
+				break;
+				default:
+					printf("Ошибка\n");
+				break;
+
+			}
 		}
 	}
 }
@@ -66,9 +103,7 @@ int main(void)
 	int i, j;
 	int sum;
 	int a;
-	char max[COMMANDS];
-
-	array_my(&array[0][0]);
+	char max[10];
 
 	a = 0;
 	tab(a);
@@ -78,10 +113,22 @@ int main(void)
 		for (j = 0; j < COMMANDS; j++) {
 			sum = sum + array[i][j];
 		}
-		printf("Команда %d набрала %d очков\n", i + 1, sum);
-		if (sum == 3 * COMMANDS) {
-			printf("Команда %d набрала максимальное число очков\n", i+1);
+		printf("Команда %s набрала %d %s\n", cmd_name[i], sum, point(sum));
+		if (sum == 3 * (COMMANDS - 1)) {
+			printf("Команда %s набрала максимальное число очков\n", cmd_name[i]);
+		}
+		max[i] = sum;
+	}
+	printf("Максимальное возможное число очков = %d\n", 3 * (COMMANDS - 1));
+
+	for (i = 0; i < 7; i++) {
+		if (max[i] > sum) {
+			sum = max[i];
+			printf("Победитель %s, набрал %d очков\n", cmd_name[i], sum);
+		} else {
+			sum = sum;
 		}
 	}
-	printf("Максимальное возможное число очков = %d\n", 3 * COMMANDS);
+
+	return 0;
 }
