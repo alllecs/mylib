fc7005c4dc6e78cc9bdf21a3a443ff5e55a9b0bb
Smirnov Alexander <alllecs@yandex.ru>
2015-03-23 10:55:41 +0300

    Рабочая версия программы

--- 10-dynamic.c
+++ 10-dynamic.c
@@ -1,21 +1,29 @@
-/* [Ссылка]
-( http://bigor.bmstu.ru/?cnt/?doc=OP2/lab10new.aux/?cou=OP2/OP_P.cou )
-Разработайте программу,
-которая читает из стандартного потока ввода строки
-и размещает их в динамических массивах строк.
-Перед размещением из строки удаляются все символы-разделители
-(пробелы и табуляция).
-Адреса этих массивов запоминаются в массиве указателей.
-После окончания ввода содержимое массивов выводится в стандартный поток вывода.
-*/
+/*
+ * Лабораторная работа 10. Динамические массивы
+ * http://bigor.bmstu.ru/?cnt/?doc=OP2/lab10new.aux/?cou=OP2/OP_P.cou
+ *
+ * Разработайте программу, которая читает из стандартного потока ввода строки
+ * и размещает их в динамических массивах строк.
+ * Перед размещением из строки удаляются все символы-разделители (пробелы
+ * и табуляция).
+ * Адреса этих массивов запоминаются в массиве указателей.
+ * После окончания ввода содержимое массивов выводится
+ * в стандартный поток вывода.
+ *
+ * Максимальное количество строк 3.
+ */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
+#define STR 3
 #define LINE 256
 
-void in(char *input1)
+char *pointers[STR];
+
+char *in(char *input1)
 {
 	int stl;
 	int i;
@@ -26,9 +34,8 @@ void in(char *input1)
 	stl = strlen(input1);
 
 	input = (char *)malloc(stl + 1);
+	bzero(input, stl + 1);
 	x = &input[0];
-
-	bzero(input, sizeof(input));
 	for (i = 0; i < stl && i < LINE; i++) {
 		c = input1[i];
 		if (c != ' ' && c != '\t') {
@@ -36,15 +43,29 @@ void in(char *input1)
 			x++;
 		}
 	}
-	puts(input);
+//	puts(input);
+	return input;
+//	free(input);
 }
-
 
 void main()
 {
 	char input1[LINE];
+	int i;
 
-	fgets(input1, LINE, stdin);
-	in(input1);
+	i = 0;
+	while (i < STR) {
+		fgets(input1, LINE, stdin);
+		pointers[i] = in(input1);
+//		printf("pointers[%d] = %p\n", i, pointers[i]);
+		i++;
+	}
 
+	i = 0;
+	while (i < STR) {
+		printf("pointers[%d] = %s", i, pointers[i]);
+		i++;
+	}
+
+	return;
 }
